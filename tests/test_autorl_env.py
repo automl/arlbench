01784e3ea04cbfaa644c3bd54cafbda0c24dd61a
Jannis Becktepe <jannis@becktepe.de>
2024-03-19 18:39:25 +0100

    tested dqn with grad info in autorl env :-)

--- tests/test_autorl_env.py
+++ tests/test_autorl_env.py
@@ -5,17 +5,13 @@ from arlbench.agents import (
     PPO,
     DQN
 )
-from arlbench.utils import (
-    make_env,
-)
 
-def test_autorl_env_ppo_cartpole():
+def test_autorl_env_ppo_grad_obs():
     CONFIG = {
         "algorithm": "ppo",
         "objective": "reward",
         "n_steps": 10,
         "n_eval_episodes": 10,
-        "track_metrics": False,
         "track_trajectories": False,
         "grad_obs": True
     }
@@ -35,11 +31,49 @@ def test_autorl_env_ppo_cartpole():
     }
 
     env = AutoRLEnv(CONFIG, envs)
+    init_obs, _ = env.reset()
+    assert init_obs.shape == (4,)
 
     action = dict(PPO.get_default_configuration())
 
-    n_obs, n_state, reward, done, _ = env.step(action)
-    print(n_obs)
-    print(reward)
+    n_obs, reward, done, _, _ = env.step(action)
+    assert n_obs.shape == (4,)
+    assert done is False
+    assert reward > 0
 
-test_autorl_env_ppo_cartpole()
+def test_autorl_env_dqn_grad_obs():
+    CONFIG = {
+        "algorithm": "dqn",
+        "objective": "reward",
+        "n_steps": 10,
+        "n_eval_episodes": 10,
+        "track_trajectories": False,
+        "grad_obs": True
+    }
+
+    cartpole = gymnax.make("CartPole-v1")
+
+    envs = {
+        0: {
+            "env": cartpole[0],
+            "env_params": cartpole[1],
+            "env_options": {
+                "n_total_timesteps": 1e5,
+                "n_env_steps": 500,
+                "n_envs": 10,
+            }
+        }
+    }
+
+    env = AutoRLEnv(CONFIG, envs)
+    init_obs, _ = env.reset()
+    assert init_obs.shape == (4,)
+
+    action = dict(DQN.get_default_configuration())
+
+    n_obs, reward, done, _, _ = env.step(action)
+    assert n_obs.shape == (4,)
+    assert done is False
+    assert reward > 0
+
+test_autorl_env_dqn_grad_obs()
