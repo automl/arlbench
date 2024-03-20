982fb235be5a3aea1b16912c6b54c6fcfa6af208
Jannis Becktepe <jannis@becktepe.de>
2024-03-20 13:47:35 +0100

    removed nested ppo and dqn

--- arlbench/agents/__init__.py
+++ arlbench/agents/__init__.py
@@ -1,14 +1,10 @@
 from .common import TimeStep
-from .dqn import make_train_dqn
-from .ppo import make_train_ppo
 from .models import Q, ActorCritic
-from .oop_ppo import PPO, PPORunnerState
-from .oop_dqn import DQN, DQNRunnerState
+from .ppo import PPO, PPORunnerState
+from .dqn import DQN, DQNRunnerState
 from .abstract_agent import Agent
 
 __all__ = [
-    "make_train_dqn",
-    "make_train_ppo",
     "Agent",
     "Q",
     "ActorCritic",
