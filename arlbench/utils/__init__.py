6b40af162e94e7b2a58227d5161b7ce707cb724b
Jannis Becktepe <jannis@becktepe.de>
2024-04-04 18:25:11 +0200

    repo cleanup

--- arlbench/utils/__init__.py
+++ arlbench/utils/__init__.py
@@ -1,6 +1,8 @@
 from .common import config_space_to_gymnasium_space
+from .handle_termination import HandleTermination
 
 
 __all__ = [
-    "config_space_to_gymnasium_space"
+    "config_space_to_gymnasium_space",
+    "HandleTermination"
 ]
