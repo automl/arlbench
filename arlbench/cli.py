212b3a5b81b9c1632be367c9e02ed1df648050e8
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2024-04-19 13:16:38 +0200

    UNTESTED: configs for hypersweeper

--- arlbench/cli.py
+++ arlbench/cli.py
@@ -10,7 +10,7 @@ from codecarbon import track_emissions
 from arlbench.arlbench import run_arlbench
 
 
-@hydra.main(version_base=None, config_path="configs", config_name="base")
+@hydra.main(version_base=None, config_path="configs", config_name="dqn_base")
 @track_emissions(offline=True, country_iso_code="DEU")
 def main(cfg):
     """Console script for arlbench."""
