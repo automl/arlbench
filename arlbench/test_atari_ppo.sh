e8077523363f1f548904361003a45a548a7b6f22
Jannis Becktepe <61006252+becktepe@users.noreply.github.com>
2024-04-20 12:01:59 +0200

    atari tests

--- arlbench/test_atari_ppo.sh
+++ arlbench/test_atari_ppo.sh
@@ -4,4 +4,4 @@
 conda activate arlb
 cd /bigwork/nhwpbecj/git_projects/arlbench/tests/sbx_comparison
 
-python arlb_ppo.py --n-eval-steps=1250 --training-steps=1250000 --env-framework=envpool --env=Pong-v5 --seed=0 --n-envs-steps=128
+python arlb_ppo.py --n-eval-steps=1250 --training-steps=1250000 --env-framework=envpool --env=Pong-v5 --seed=0 --n-env-steps=128
