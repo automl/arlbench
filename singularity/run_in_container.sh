bbf646601125b70a59dd9b6d618ab27094644c4c
Jannis Becktepe <61006252+becktepe@users.noreply.github.com>
2024-04-23 22:56:55 +0200

    singularity

--- singularity/run_in_container.sh
+++ singularity/run_in_container.sh
@@ -2,4 +2,4 @@
 conda activate arlb
 
 # code to run your program
-python arlb_dqn.py --training-steps=125000 --n-eval-steps=100 --n-eval-episodes=10 --dir-name=arlb --n-envs=1 --env-framework=envpool --env=Pong-v5 --seed=$1
+python arlb_dqn.py --training-steps=125000 --n-eval-steps=125 --n-eval-episodes=10 --dir-name=arlb --n-envs=10 --env-framework=envpool --env=Pong-v5 --seed=0
