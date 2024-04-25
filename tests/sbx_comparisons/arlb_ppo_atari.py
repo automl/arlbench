f7c3653feec1a85a3d463dbe71b75a0f54effff2
Julian Dierkes <dierkes-julian@web.de>
2024-04-25 16:23:41 +0200

    sb3 zoo parameters

--- tests/sbx_comparisons/arlb_ppo_atari.py
+++ tests/sbx_comparisons/arlb_ppo_atari.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import os
 import time
+import sys
 
 import jax
 import pandas as pd
@@ -18,21 +19,21 @@ def ppo_runner(dir_name, log, framework, env_name, config, training_kw_args, see
     eval_env = make_env(framework, env_name, n_envs=training_kw_args["n_eval_episodes"], seed=seed, cnn_policy=cnn_policy)
     rng = jax.random.PRNGKey(seed)
 
-    print(jax.devices())
-    print(jax.default_backend())
-    print("test!!!")
+    log.info(jax.devices())
+    log.info(jax.default_backend())
+    log.info("test!!!")
 
     hpo_config = PPO.get_default_hpo_config()
     hpo_config["minibatch_size"] = 256
-    hpo_config["update_epochs"] = 4
+    hpo_config["update_epochs"] = 3
     hpo_config["gamma"] = 0.99
     hpo_config["gae_lambda"] = 0.95
     hpo_config["ent_coef"] = 0.01
     hpo_config["max_grad_norm"] = 0.5
     hpo_config["clip_eps"] = 0.2
     hpo_config["n_steps"] = 128
-    hpo_config["vf_coef"] = 1.0
-    #hpo_config["lr"] = 2.5e-4
+    hpo_config["vf_coef"] = 0.5
+    hpo_config["lr"] = 2.5e-4
 
     nas_config = PPO.get_default_nas_config()
     nas_config["activation"] = "relu"
@@ -51,7 +52,6 @@ def ppo_runner(dir_name, log, framework, env_name, config, training_kw_args, see
     std_return = result.eval_rewards.std(axis=1)
     str_results = [f"{mean:.2f}+-{std:.2f}" for mean, std in zip(mean_return, std_return)]
     log.info(f"{training_time}, {str_results}")
-    print(f"{training_time}, {str_results}")
 
     train_info_df = pd.DataFrame()
     for i in range(len(mean_return)):
@@ -71,9 +71,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir-name", type=str, default="test")
     parser.add_argument("--training-steps", type=int, default=10000000)
-    parser.add_argument("--n-eval-steps", type=int, default=100)
-    parser.add_argument("--n-eval-episodes", type=int, default=10)
-    parser.add_argument("--n-envs", type=int, default=8)
+    parser.add_argument("--n-eval-steps", type=int, default=10)
+    parser.add_argument("--n-eval-episodes", type=int, default=128)
+    parser.add_argument("--n-envs", type=int, default=16)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--env-framework", type=str, default="envpool")
     parser.add_argument("--env", type=str, default="Pong-v5")
@@ -82,7 +82,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     logger = logging.getLogger(__name__)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(formatter)
     logger.setLevel(logging.INFO)
+    logger.addHandler(stdout_handler)
 
     training_kw_args = {
         "n_total_timesteps": args.training_steps,
