9af6edcc00df65a85d2e0cd5b524cadd63c11027
dierkes-j <dierkes-julian@web.de>
2024-04-29 00:20:13 +0200

    make video

--- tests/sbx_comparisons/envpool_test.py
+++ tests/sbx_comparisons/envpool_test.py
@@ -2,14 +2,13 @@ import envpool
 import jax
 import jax.numpy as jnp
 import numpy as np
+import cv2
 
 env = envpool.make("Breakout-v5", env_type="gym", num_envs=8, seed=42, episodic_life=False)
 handle1, _, _, xla_step = env.xla()
 
 action = jnp.array([0, 0, 0, 0, 0, 0, 0, 0])
 np_action = np.array([0, 0, 0, 0, 0, 0, 0, 0])
-import ipdb
-ipdb.set_trace()
 output = env.reset()
 
 reset_shape = jax.tree_map(lambda x: x[:1], output)
@@ -19,8 +18,29 @@ single_step_shape = jax.tree_map(lambda x: x[:1], step_shape)
 output = env.reset()
 
 
-import ipdb
-ipdb.set_trace()
+img_list = []
+for i in range(27000):
+    handle1, (obs1, reward1, done1, trunc1, info1) = xla_step(handle1, action)
+    # store obs as image
+    np_obs = obs1.numpy()
+    scaled_obs = (255 * np_obs).astype(np.uint8)  # Scale to 0-255 as uint8
+    img_list.append(scaled_obs)
+    if done1:  # Check if the environment says the episode is complete
+        print("early stopping")
+        break
+
+video_filename = 'output_video.mp4'
+height, width = img_list[0].shape
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Using DIVX codec
+video = cv2.VideoWriter(video_filename, fourcc, 30, (width, height), isColor=False)
+# Write each frame to the video
+for img in img_list:
+    video.write(img)
+
+# Release the video writer
+video.release()
+
+
 handle1, (obs1, reward1, done1, trunc1, info1) = xla_step(handle1, action)
 obs2, reward2, done2, trunc2, info2 = jax.experimental.io_callback(env.step, single_step_shape, np_action[:1], env_id=np.array([0]))
 #obs1, reward1, done1, trunc1, info1 = env.step(np_action)
