3b9ec4d1900d96c919ebf1b284d0e6af96620feb
dierkes-j <dierkes-julian@web.de>
2024-05-02 17:58:59 +0200

    remove uncommenting tests

--- tests/core/algorithms/test_sac.py
+++ tests/core/algorithms/test_sac.py
@@ -43,60 +43,60 @@ def test_normalize_observations_sac(n_envs=N_ENVS):
 
 
 # Default SAC
-#def test_default_sac(n_envs=N_ENVS):
-#    env = make_env("gymnax", "Pendulum-v1", seed=42, n_envs=n_envs)
-#    eval_env = make_env("gymnax", "Pendulum-v1", seed=42, n_envs=EVAL_EPISODES)
-#    rng = jax.random.PRNGKey(42)
-#
-#    config = SAC.get_default_hpo_config()
-#    nas_config = SAC.get_default_nas_config()
-#    nas_config["hidden_size"] = 64
-#    agent = SAC(config, env, eval_env=eval_env, nas_config=nas_config)
-#    algorithm_state = agent.init(rng)
-#
-#    start = time.time()
-#    algorithm_state, results = agent.train(
-#        *algorithm_state,
-#        n_total_timesteps=TRAINING_STEPS,
-#        n_eval_steps=EVAL_STEPS,
-#        n_eval_episodes=EVAL_EPISODES,
-#    )
-#    training_time = time.time() - start
-#    reward = results.eval_rewards[-1].mean()
-#
-#    print(
-#        f"n_envs = {n_envs}, time = {training_time:.2f}, env_steps = {n_envs * algorithm_state.runner_state.global_step}, updates = {algorithm_state.runner_state.global_step}, reward = {reward:.2f}"
-#    )
-#    assert reward > -300
-#
-#
-## Gradient steps
-#def test_gradient_steps_sac(n_envs=N_ENVS):
-#    env = make_env("gymnax", "Pendulum-v1", seed=42, n_envs=n_envs)
-#    eval_env = make_env("gymnax", "Pendulum-v1", seed=42, n_envs=EVAL_EPISODES)
-#    rng = jax.random.PRNGKey(42)
-#
-#    config = SAC.get_default_hpo_config()
-#    config["gradient_steps"] = 4
-#    nas_config = SAC.get_default_nas_config()
-#    nas_config["hidden_size"] = 64
-#    agent = SAC(config, env, eval_env=eval_env, nas_config=nas_config)
-#    algorithm_state = agent.init(rng)
-#
-#    start = time.time()
-#    algorithm_state, results = agent.train(
-#        *algorithm_state,
-#        n_total_timesteps=TRAINING_STEPS,
-#        n_eval_steps=EVAL_STEPS,
-#        n_eval_episodes=EVAL_EPISODES,
-#    )
-#    training_time = time.time() - start
-#    reward = results.eval_rewards[-1].mean()
-#
-#    print(
-#        f"n_envs = {n_envs}, time = {training_time:.2f}, env_steps = {n_envs * algorithm_state.runner_state.global_step}, updates = {algorithm_state.runner_state.global_step}, reward = {reward:.2f}"
-#    )
-#    assert reward > -300
+def test_default_sac(n_envs=N_ENVS):
+    env = make_env("gymnax", "Pendulum-v1", seed=42, n_envs=n_envs)
+    eval_env = make_env("gymnax", "Pendulum-v1", seed=42, n_envs=EVAL_EPISODES)
+    rng = jax.random.PRNGKey(42)
+
+    config = SAC.get_default_hpo_config()
+    nas_config = SAC.get_default_nas_config()
+    nas_config["hidden_size"] = 64
+    agent = SAC(config, env, eval_env=eval_env, nas_config=nas_config)
+    algorithm_state = agent.init(rng)
+
+    start = time.time()
+    algorithm_state, results = agent.train(
+        *algorithm_state,
+        n_total_timesteps=TRAINING_STEPS,
+        n_eval_steps=EVAL_STEPS,
+        n_eval_episodes=EVAL_EPISODES,
+    )
+    training_time = time.time() - start
+    reward = results.eval_rewards[-1].mean()
+
+    print(
+        f"n_envs = {n_envs}, time = {training_time:.2f}, env_steps = {n_envs * algorithm_state.runner_state.global_step}, updates = {algorithm_state.runner_state.global_step}, reward = {reward:.2f}"
+    )
+    assert reward > -300
+
+
+# Gradient steps
+def test_gradient_steps_sac(n_envs=N_ENVS):
+    env = make_env("gymnax", "Pendulum-v1", seed=42, n_envs=n_envs)
+    eval_env = make_env("gymnax", "Pendulum-v1", seed=42, n_envs=EVAL_EPISODES)
+    rng = jax.random.PRNGKey(42)
+
+    config = SAC.get_default_hpo_config()
+    config["gradient_steps"] = 4
+    nas_config = SAC.get_default_nas_config()
+    nas_config["hidden_size"] = 64
+    agent = SAC(config, env, eval_env=eval_env, nas_config=nas_config)
+    algorithm_state = agent.init(rng)
+
+    start = time.time()
+    algorithm_state, results = agent.train(
+        *algorithm_state,
+        n_total_timesteps=TRAINING_STEPS,
+        n_eval_steps=EVAL_STEPS,
+        n_eval_episodes=EVAL_EPISODES,
+    )
+    training_time = time.time() - start
+    reward = results.eval_rewards[-1].mean()
+
+    print(
+        f"n_envs = {n_envs}, time = {training_time:.2f}, env_steps = {n_envs * algorithm_state.runner_state.global_step}, updates = {algorithm_state.runner_state.global_step}, reward = {reward:.2f}"
+    )
+    assert reward > -300
 
 if __name__ == "__main__":
     with warnings.catch_warnings():
