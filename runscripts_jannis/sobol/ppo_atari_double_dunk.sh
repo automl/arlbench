250e27a2776f638dcb775b18d06992ebab7619b7
Jannis Becktepe <jannis@becktepe.de>
2024-05-06 22:54:30 +0200

    feat: runscripts for random search

---
+++ runscripts_jannis/sobol/ppo_atari_double_dunk.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH -n 16                      
+#SBATCH --mem 64GB                                      
+#SBATCH -J arlb_rs_ppo_atari_double_dunk                                 
+#SBATCH -A hpc-prf-intexml                              # TODO check for your project
+#SBATCH -t 7-00:00:00                                   # TODO check for your clusters time limit
+#SBATCH --mail-type fail                                
+#SBATCH --mail-user becktepe@stud.uni-hannover.de       # TODO enter your mail and hope slurm isn't reaching out to you :D
+#SBATCH -p normal                                       # TODO check for your clusters partition
+#SBATCH --output sobol/log/arlb_rs_ppo_atari_double_dunk_%A_%a.out
+#SBATCH --error sobol/log/arlb_rs_ppo_atari_double_dunk_%A_%a.err
+#SBATCH --array=0-9
+
+cd ..
+python runscripts/run_arlbench.py -m --config-name=random_runs autorl.seed=$SLURM_ARRAY_TASK_ID algorithm=ppo search_space=ppo environment=atari_double_dunk cluster=pc2_gpu
+
