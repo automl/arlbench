e19f06ba6253d24e79bcd184efeaa34da1630237
Jannis Becktepe <jannis@becktepe.de>
2024-05-08 22:10:54 +0200

    fix: separate search spaces

--- runscripts/create_configs.py
+++ runscripts/create_configs.py
@@ -4,16 +4,17 @@ from arlbench.core.algorithms import DQN, PPO, SAC
 
 def create_algorithm_configs(seed: int = 0):
     for algorithm in [DQN, PPO, SAC]:
+        hp_search_space = algorithm.get_hpo_search_space()
         hp_config_space = algorithm.get_hpo_config_space()
-        nas_config_sapce = algorithm.get_nas_config_space()
-        search_space = config_space_to_yaml(hp_config_space, config_key="hp_config", seed=seed)
+        nas_config_space = algorithm.get_nas_config_space()
+        search_space = config_space_to_yaml(hp_search_space, config_key="hp_config", seed=seed)
         with open(
             f"./runscripts/configs/search_space/{algorithm.name}.yaml", "w"
         ) as yaml_file:
             yaml_file.write(search_space)
 
         default_config = save_defaults_to_yaml(
-            hp_config_space, nas_config_sapce, algorithm.name
+            hp_config_space, nas_config_space, algorithm.name
         )
         default_config = "# @package _global_\n" + default_config
 
