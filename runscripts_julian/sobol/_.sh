6b8d5597e497a8ad150600d1cf58713c8101dcd0
Julian Dierkes <dierkes-julian@web.de>
2024-05-09 12:55:49 +0200

    clean up

--- runscripts_julian/sobol/_.sh
+++ runscripts_julian/sobol/_.sh
@@ -4,14 +4,14 @@
 #SBATCH --mem-per-cpu=2000M                                      
 #SBATCH --job-name=rs_atari
 ###SBATCH --account=
-#SBATCH -t 128:00:00                                   # TODO check for your clusters time limit
+#SBATCH -t 128:00:00            
 #SBATCH --mail-type fail                                
-#SBATCH --mail-user becktepe@stud.uni-hannover.de       # TODO enter your mail and hope slurm isn't reaching out to you :D
+#SBATCH --mail-user dierkes@aim.rwth-aachen.de
 #SBATCH --output sobol/log/arlb_rs___%A_%a.out
 #SBATCH --error sobol/log/arlb_rs___%A_%a.err
-#SBATCH --array=0-9
+#SBATCH --array 0-10%2
 
 cd ..
 source /rwthfs/rz/cluster/home/oh751555/i14/arlbench/.venv/bin/activate
-python runscripts/run_arlbench.py -m --config-name=random_runs autorl.seed=$SLURM_ARRAY_TASK_ID algorithm= search_space= environment= cluster=
+python runscripts/run_arlbench.py -m --config-name=random_runs autorl.seed=$SLURM_ARRAY_TASK_ID algorithm= search_space=_8env environment= cluster= +experiments=_ 
 
--- runscripts_julian/sobol/ppo_atari_breakout.sh
+++ runscripts_julian/sobol/ppo_atari_breakout.sh
@@ -7,11 +7,11 @@
 #SBATCH -t 128:00:00            
 #SBATCH --mail-type fail                                
 #SBATCH --mail-user dierkes@aim.rwth-aachen.de
-#SBATCH --output sobol/log/arlb_rs_ppo_atari_pong_%A_%a.out
-#SBATCH --error sobol/log/arlb_rs_ppo_atari_pong_%A_%a.err
+#SBATCH --output sobol/log/arlb_rs_ppo_atari_breakout_%A_%a.out
+#SBATCH --error sobol/log/arlb_rs_ppo_atari_breakout_%A_%a.err
 #SBATCH --array 0-10%2
 
 cd ..
 source /rwthfs/rz/cluster/home/oh751555/i14/arlbench/.venv/bin/activate
-python runscripts/run_arlbench.py -m --config-name=random_runs autorl.seed=$SLURM_ARRAY_TASK_ID algorithm=ppo search_space=ppo_8env environment=atari_pong cluster=claix_gpu_dgx +experiments=atari_pong_ppo 
+python runscripts/run_arlbench.py -m --config-name=random_runs hydra.launcher.array_parallelism=256 autorl.seed=$SLURM_ARRAY_TASK_ID algorithm=ppo search_space=ppo_8env environment=atari_breakout cluster=claix_gpu_h100 +experiments=atari_breakout_ppo 
 
