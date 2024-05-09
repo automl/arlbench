246627e6345bf8953d158ab136639e7d91030b68
Jannis Becktepe <jannis@becktepe.de>
2024-05-09 18:26:41 +0200

    feat: luis atari + cc scripts

---
+++ runscripts_jannis/rs_pc2_atari.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#./run_rs_pc2.sh atari_battle_zone_dqn pc2_gpu
+# ./run_rs_pc2.sh atari_double_dunk_dqn pc2_gpu
+
+ ./run_rs_pc2.sh atari_battle_zone_ppo pc2_gpu # -> started 8.5. around 22:17
+# ./run_rs_pc2.sh atari_double_dunk_ppo pc2_gpu
