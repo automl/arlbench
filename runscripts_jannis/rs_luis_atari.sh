322365e818b0b51606a18d9472552bf253fa9fda
NHWP25179 <nhwpbecj@ai-n001.css.lan>
2024-05-10 12:03:42 +0200

    feat: luis classic control scripts

--- runscripts_jannis/rs_luis_atari.sh
+++ runscripts_jannis/rs_luis_atari.sh
@@ -3,4 +3,4 @@
 # ./rs_luis.sh atari_double_dunk_dqn pc2_gpu
 
 # ./rs_luis.sh atari_battle_zone_ppo pc2_gpu # -> started 8.5. around 22:17
-# ./rs_luis.sh atari_double_dunk_ppo pc2_gpu
+./rs_luis.sh atari_double_dunk_ppo pc2_gpu
