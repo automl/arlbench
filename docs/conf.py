1378d94c74be62ea4764e5362f91d64fc6dc82d5
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2024-05-16 15:09:40 +0200

    fix naming error

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,10 @@
 import automl_sphinx_theme
 
-from arlbench import copyright, __author__, __version__, __name__
+from arlbench import __copyright__, __author__, __version__, __name__
 
 
 options = {
-    "copyright": copyright,
+    "copyright": __copyright__,
     "author": __author__,
     "version": __version__,
     "name": __name__,
