7e5c619e2d26f47e4f9c1a8553bbeacba998962b
Jannis Becktepe <jannis@becktepe.de>
2024-05-16 15:04:41 +0200

    brax configs + updated random search

--- runscripts_jannis/rs_pc2_local_gpu.sh
+++ runscripts_jannis/rs_pc2_local_gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# USAGE run_rs.sh EXPERIMENT      CLUSTER 
-# USAGE run_rs.sh cc_cartpole_dqn local  
+# USAGE run_rs.sh EXPERIMENT      
+# USAGE run_rs.sh cc_cartpole_dqn  
 
 directory="sobol"
 
@@ -10,21 +10,22 @@ mkdir -p "$directory/log"
 echo "#!/bin/bash
 
 
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:a100:1
 #SBATCH --mem=16GB
 #SBATCH -J arlb_rs_${1}
 #SBATCH -A hpc-prf-intexml                              # TODO check for your project
-#SBATCH -t 7-00:00:00                                   # TODO check for your clusters time limit
+#SBATCH -t 2-00:00:00                                   # TODO check for your clusters time limit
 #SBATCH --mail-type fail
 #SBATCH --mail-user becktepe@stud.uni-hannover.de       # TODO enter your mail and hope slurm isn't reaching out to you :D
-#SBATCH -p normal                                       # TODO check for your clusters partition
+#SBATCH -p gpu                                       # TODO check for your clusters partition
 #SBATCH --output $directory/log/arlb_rs_${1}_%A_%a.out
 #SBATCH --error $directory/log/arlb_rs_${1}_%A_%a.err
-#SBATCH --array=1-9
+#SBATCH --array=0-9
 
-sleep \$SLURM_ARRAY_TASK_ID
+
 cd ..
-python runscripts/run_arlbench.py -m --config-name=random_runs "autorl.seed=\$SLURM_ARRAY_TASK_ID" "experiments=$1" "cluster=$2" 
+python runscripts/run_arlbench.py -m --config-name=random_runs "autorl.seed=\$SLURM_ARRAY_TASK_ID" "experiments=$1" "cluster=local" 
 " > $directory/${1}.sh
 echo "Submitting $directory for $1"
 chmod +x $directory/${1}.sh
