146a1872a62e1604bf4a853793e5259a9fe8723f
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2024-05-16 15:16:46 +0200

    fix: docs config examples

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,18 @@ options = {
     "html_theme_options": {
         "github_url": "https://github.com/automl/ARLBench",
         "twitter_url": "https://twitter.com/automl_org?lang=de",
-    }
+    },
+    "extensions": [
+        "myst_parser",
+        "sphinx.ext.autodoc",
+        "sphinx.ext.viewcode",
+        "sphinx.ext.napoleon",  # Enables to understand NumPy docstring
+        # "numpydoc",
+        "sphinx.ext.autosummary",
+        "sphinx.ext.autosectionlabel",
+        "sphinx_autodoc_typehints",
+        "sphinx.ext.doctest",
+    ],
 }
 
 automl_sphinx_theme.set_options(globals(), options)
