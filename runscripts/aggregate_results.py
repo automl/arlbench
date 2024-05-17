6e4b4964198fa08f5f5c8689f182245cd9dbb1bf
Jannis Becktepe <jannis@becktepe.de>
2024-05-17 17:39:03 +0200

    initial designs for most experiments

--- runscripts/aggregate_results.py
+++ runscripts/aggregate_results.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import logging
+import shutil
 
 
 def aggregate_runhistories(approach: str):
@@ -18,17 +19,37 @@ def aggregate_runhistories(approach: str):
             if os.path.exists(combined_path):
                 logging.info(f"Aggregated runhistory already exists for {algorithm_env}. Skipping...")
                 #continue
-            
+        
             all_data = []
             for seed_dir in os.listdir(agent_env_path):
                 seed_path = os.path.join(agent_env_path, seed_dir)
                 if os.path.isdir(seed_path):
                     csv_file = os.path.join(seed_path, "runhistory.csv")
-                    print(csv_file)
                     if os.path.isfile(csv_file):
                         data = pd.read_csv(csv_file)
                         data["seed"] = seed_dir
                         all_data.append(data)
+
+                    # remove directories > 255 because we had to many random runs
+                    # for some environments due to a bug in the hypersweeper
+                    for run_dir in os.listdir(seed_path):
+                        run_path = os.path.join(seed_path, run_dir)
+                        if not os.path.isdir(run_path):
+                            continue
+                        
+                        # We do not need this directory
+                        if run_dir == "checkpoints":
+                            print(f"Removing {run_path}")
+                            shutil.rmtree(run_path)
+                            continue
+
+                        try:
+                            run_dir_num = int(run_dir)
+                            if run_dir_num > 255:
+                               print(f"Removing {run_path}")
+                               shutil.rmtree(run_path)
+                        except:
+                            pass
             
             if all_data:
                 combined_data = pd.concat(all_data, ignore_index=True)
