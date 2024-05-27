b8551dd9acf493febc1b689d1e9ce0a8bb490883
Jannis Becktepe <jannis@becktepe.de>
2024-05-27 17:35:14 +0200

    added runscripts for SMAC

--- runscripts_jannis/rs_pc2_brax.sh
+++ runscripts_jannis/rs_pc2_brax.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # ./rs_pc2.sh brax_ant_ppo pc2_gpu
-#./rs_pc2.sh brax_halfcheetah_ppo pc2_gpu
-./rs_pc2.sh brax_hopper_ppo pc2_gpu
-./rs_pc2.sh brax_humanoid_ppo pc2_gpu
+# ./rs_pc2.sh brax_halfcheetah_ppo pc2_gpu
+# ./rs_pc2.sh brax_hopper_ppo pc2_gpu
+# ./rs_pc2.sh brax_humanoid_ppo pc2_gpu
--- runscripts_jannis/sobol/cc_acrobot_dqn.sh
+++ runscripts_jannis/sobol/cc_acrobot_dqn.sh
@@ -4,14 +4,16 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=64GB
 #SBATCH -J arlb_rs_cc_acrobot_dqn
-#SBATCH -t 4-00:00:00                                   # TODO check for your clusters time limit
+#SBATCH -A hpc-prf-intexml                              # TODO check for your project
+#SBATCH -t 7-00:00:00                                   # TODO check for your clusters time limit
 #SBATCH --mail-type fail
 #SBATCH --mail-user becktepe@stud.uni-hannover.de       # TODO enter your mail and hope slurm isn't reaching out to you :D
-#SBATCH -p ai                                           # TODO check for your clusters partition
+#SBATCH -p normal                                       # TODO check for your clusters partition
 #SBATCH --output sobol/log/arlb_rs_cc_acrobot_dqn_%A_%a.out
 #SBATCH --error sobol/log/arlb_rs_cc_acrobot_dqn_%A_%a.err
-#SBATCH --array=0-0
+#SBATCH --array=0-9
+
 
 cd ..
-python runscripts/run_arlbench.py -m --config-name=random_runs autorl.seed=$SLURM_ARRAY_TASK_ID +experiments=cc_acrobot_dqn cluster=local 
+python runscripts/run_arlbench.py -m --config-name=random_runs autorl.seed=$SLURM_ARRAY_TASK_ID experiments=cc_acrobot_dqn cluster=local 
 
