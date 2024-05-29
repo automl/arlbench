69b49482761492333d22f12276df5b684180dd63
Jannis Becktepe <jannis@becktepe.de>
2024-05-29 10:35:54 +0200

    runscripts for PC2

--- runscripts_jannis/pbt_pc2_classic_control.sh
+++ runscripts_jannis/pbt_pc2_classic_control.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-./pbt_pc2_local.sh cc_acrobot_dqn
-./pbt_pc2_local.sh cc_cartpole_dqn
-./pbt_pc2_local.sh cc_mountain_car_dqn
+./pbt_pc2_local.sh cc_acrobot_dqn_pbt
+./pbt_pc2_local.sh cc_cartpole_dqn_pbt
+./pbt_pc2_local.sh cc_mountain_car_dqn_pbt
 
 ./pbt_pc2_local.sh cc_acrobot_ppo
 ./pbt_pc2_local.sh cc_cartpole_ppo
@@ -9,5 +9,5 @@
 ./pbt_pc2_local.sh cc_mountain_car_ppo
 ./pbt_pc2_local.sh cc_pendulum_ppo
 
-./pbt_pc2_local.sh cc_continuous_mountain_car_sac
-./pbt_pc2_local.sh cc_pendulum_sac
+./pbt_pc2_local.sh cc_continuous_mountain_car_sac_pbt
+./pbt_pc2_local.sh cc_pendulum_sac_pbt
