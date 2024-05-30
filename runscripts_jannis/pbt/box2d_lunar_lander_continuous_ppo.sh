4523f4089af870fb78631fca0cedffeaa819f44c
Jannis Becktepe <jannis@becktepe.de>
2024-05-30 10:08:34 +0200

    optimizer results

--- runscripts_jannis/pbt/box2d_lunar_lander_continuous_ppo.sh
+++ runscripts_jannis/pbt/box2d_lunar_lander_continuous_ppo.sh
@@ -2,18 +2,18 @@
 
 
 #SBATCH --cpus-per-task=64
-#SBATCH --gres=gpu:a100:1
 #SBATCH --mem=128GB
 #SBATCH -J pbt_box2d_lunar_lander_continuous_ppo
 #SBATCH -A hpc-prf-intexml                              # TODO check for your project
-#SBATCH -t 2-00:00:00                                   # TODO check for your clusters time limit
+#SBATCH -t 7-00:00:00                                   # TODO check for your clusters time limit
 #SBATCH --mail-type fail
 #SBATCH --mail-user becktepe@stud.uni-hannover.de       # TODO enter your mail and hope slurm isn't reaching out to you :D
-#SBATCH -p gpu                                       # TODO check for your clusters partition
-#SBATCH --output pbt/log/pbt_box2d_lunar_lander_continuous_ppo_%A.out
-#SBATCH --error pbt/log/pbt_box2d_lunar_lander_continuous_ppo_%A.err
+#SBATCH -p normal                                       # TODO check for your clusters partition
+#SBATCH --output pbt/log/pbt_box2d_lunar_lander_continuous_ppo_%A_%a.out
+#SBATCH --error pbt/log/pbt_box2d_lunar_lander_continuous_ppo_%A_%a.err
+#SBATCH --array=0-2
 
 
 cd ..
-python runscripts/run_arlbench.py -m --config-name=tune_pbt experiments=box2d_lunar_lander_continuous_ppo cluster=local
+python runscripts/run_arlbench.py -m --config-name=tune_pbt experiments=box2d_lunar_lander_continuous_ppo cluster=local pbt_seed=$SLURM_ARRAY_TASK_ID
 
--- runscripts_jannis/pbt/box2d_lunar_lander_ppo.sh
+++ runscripts_jannis/pbt/box2d_lunar_lander_ppo.sh
@@ -9,10 +9,11 @@
 #SBATCH --mail-type fail
 #SBATCH --mail-user becktepe@stud.uni-hannover.de       # TODO enter your mail and hope slurm isn't reaching out to you :D
 #SBATCH -p normal                                       # TODO check for your clusters partition
-#SBATCH --output pbt/log/pbt_box2d_lunar_lander_ppo_%A.out
-#SBATCH --error pbt/log/pbt_box2d_lunar_lander_ppo_%A.err
+#SBATCH --output pbt/log/pbt_box2d_lunar_lander_ppo_%A_%a.out
+#SBATCH --error pbt/log/pbt_box2d_lunar_lander_ppo_%A_%a.err
+#SBATCH --array=0-2
 
 
 cd ..
-python runscripts/run_arlbench.py -m --config-name=tune_pbt experiments=box2d_lunar_lander_ppo cluster=local
+python runscripts/run_arlbench.py -m --config-name=tune_pbt experiments=box2d_lunar_lander_ppo cluster=local pbt_seed=$SLURM_ARRAY_TASK_ID
 
