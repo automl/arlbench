345fba5de371f0e597fdd20553482d410117f3b0
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2024-05-30 11:05:09 +0200

    added pbt set to runscript

--- runscripts_jannis/pbt_pc2_atari.sh
+++ runscripts_jannis/pbt_pc2_atari.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./pbt_pc2.sh atari_battle_zone_ppo pc2_gpu
-./pbt_pc2.sh atari_double_dunk_ppo pc2_gpu
-./pbt_pc2.sh atari_phoenix_ppo pc2_gpu
-./pbt_pc2.sh atari_qbert_ppo pc2_gpu
-./pbt_pc2.sh atari_this_game_ppo pc2_gpu
+#./pbt_pc2.sh atari_double_dunk_ppo pc2_gpu
+#./pbt_pc2.sh atari_phoenix_ppo pc2_gpu
+#./pbt_pc2.sh atari_qbert_ppo pc2_gpu
+#./pbt_pc2.sh atari_this_game_ppo pc2_gpu
 
