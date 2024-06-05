29ba718b085b4d96c2c74332dfac50c2ec1d551e
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2024-06-05 14:11:45 +0200

    Fix: examples

--- arlbench/__init__.py
+++ arlbench/__init__.py
@@ -7,7 +7,7 @@ from .autorl.autorl_env import AutoRLEnv
 
 __author__ = """AutoML Hannover"""
 __email__ = """automl@ai.uni-hannover.de"""
-__version__ = """0.1.0"""
+__version__ = """0.1.1"""
 __copyright__ = f"Copyright {datetime.date.today().strftime('%Y')}, AutoML Hannover"
 
 __all__ = ["AutoRLEnv", "run_arlbench"]
